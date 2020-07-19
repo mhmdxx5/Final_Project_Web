0caad3462f27a8ce94fe377cf0408b75e74b6f99
mhmdxx5 <48560391+mhmdxx5@users.noreply.github.com>
2020-07-20 02:53:04 +0300

    Update models.py

--- project/models.py
+++ project/models.py
@@ -3,13 +3,6 @@ from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
-from .managers import PersonManager
 
-class Person(User):
-    objects = PersonManager()
-
-    class Meta:
-        proxy = True
-        ordering = ('typeof', )
 
 
