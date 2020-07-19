d24f4336a9d9293303749344c2cc55ed45ea3c33
mhmdxx5 <48560391+mhmdxx5@users.noreply.github.com>
2020-07-20 02:50:18 +0300

    Update models.py

--- project/models.py
+++ project/models.py
@@ -2,5 +2,14 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
+from django.contrib.auth.models import User
+from .managers import PersonManager
+
+class Person(User):
+    objects = PersonManager()
+
+    class Meta:
+        proxy = True
+        ordering = ('typeof', )
 
 
